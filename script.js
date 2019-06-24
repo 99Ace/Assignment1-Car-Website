c5a49b36be5c931f9e84d28b2c62cff3e538e812
Ace Ang <chunliang.ang@gmail.com>
2019-06-24 14:23:08 +0000

    landing page append fade stuck

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
  /*global $*/
-// $(function(){
-// })
+// Set array of choices for colors, fonts, position, 
+let color_choice = [ //contain 4 colors
+    "red",
+    "blue",
+    "green",
+    "yellow"
+]
+let car_brands = [ // contain car brands for display
+    "Audi",
+    "BMW",
+    "Mercedes",
+    "Toyota",
+    "Honda",
+    "Nissan",
+    "Ferrari",
+    "Maserati",
+    "Porsche",
+    "Citroen",
+    "Renault",
+    "Volkswagen",
+    "Volvo",
+    "Jaguar"
+]
 
-function test() {
+// create functions for multiple usage
+function testing() {
     alert ("test function");
 }
-function random_number() {
-    let random_text = Math.floor(Math.random()*50)+1;
+function generate_random_number(length) { // create random number
+    let random_text = Math.floor(Math.random()*length);
     // alert (random_text);
     return random_text;
 }
 function print_text(words) {
-    $("#animi").text(words);
-    // alert ("");
+    // alert("got run")
+    $("#animi").append(`<div class="display">${words}</div>`)
+    $("#animi").children(0).fadeOut(100000,"swing");
 }
  
-let num = random_number();
-print_text(num)
+let num = generate_random_number(car_brands.length);
+print_text(car_brands[num]);
